ccc2ad43d208459d0dac0a941fa4930444f10519
radu <radu@Mac-mini-di-radu.local>
2022-12-08 13:20:47 +0100

    replace fake crud with real database interaction

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,45 +1,85 @@
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  // res.status(200).json({
-  //   status: 'success',
-  //   results: tours.length,
-  //   data: { tours },
-  // });
-};
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = Tour.find();
 
-exports.getTour = (req, res) => {
-  // const tour = tours.find((el) => el.id === parseInt(req.params.id));
-  // res.status(200).json({
-  //   data: { tours: tour },
-  // });
-};
-
-exports.createTour = (req, res) => {};
-
-exports.updateTour = (req, res) => {
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour: {},
-  //   },
-  // });
-};
-
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    // null means here that the resource which was deleted no longer exists
-    data: null,
-  });
-};
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: { tours },
+    });
+  } catch (err) {
+    res.status(404).json({
       status: 'fail',
-      message: 'Missing name or price parameters',
+      message: 'Invalid data sent!',
     });
   }
-  next();
+};
+
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      data: { tour },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
+};
+
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    return res.status(200).json({
+      status: 'success',
+      data: { tour: newTour },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
+};
+
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: { tour },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
+};
+
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      // null means here that the resource which was deleted no longer exists
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
